f43789f1447bd7df7af29aa73e4379be4159525b
Kirill <syskirill.com>
2025-05-06 09:23:45 +0300

    Initial Commit

---
+++ Explore Minsk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Explore Minsk
+//
+//  Created by Кирилл Сысоев on 06.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
